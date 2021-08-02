e4de74e090c12faa10bc103fdb000d637b318999
romka <8026787@gmail.com>
2021-08-02 13:33:23 +0300

    fix thread safe

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
 
@@ -35,19 +37,17 @@ fun main() {
         ctx.json(mapOf("error" to e.message))
     }
 
-    val futures: MutableMap<String, CompletableFuture<Response>> = HashMap()
+    val futures: ConcurrentMap<String, CompletableFuture<Response>> = ConcurrentHashMap()
 
     client.subscribe("#") { topic, message ->  futures[topic]?.complete(Response(message.toString())) }
-
-    fun waitFor(key: String) = CompletableFuture<Response>().apply {
-        futures[key] = this;
-    }
 
     app.post("/") { ctx ->
         val body = ctx.bodyAsClass(Request::class.java)
         if (body.ack_topic != null) {
             try{
-                ctx.json(waitFor(body.ack_topic).get(body.ack_timeout, TimeUnit.SECONDS))
+                ctx.json(CompletableFuture<Response>().apply {
+                    futures[body.ack_topic] = this;
+                }.get(body.ack_timeout, TimeUnit.SECONDS))
             } finally {
                 futures.remove(body.ack_topic)
             }
